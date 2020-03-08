ac7a2c02f7934e6d96a811b18028f92d40d67bfa
AlexPiguet <alexandre.piguet@outlook.com>
2020-03-08 22:54:44 +0100

    Details_Activity Not working for the moment

---
+++ app/src/main/java/com/example/wikitea/TeaDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.wikitea;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeaDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tea_type);
+    }
+
+}
