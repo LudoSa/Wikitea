d0ff197fd0f881079aaa6437c26ccb083e8d7222
LudoSa <ludovic.sahraoui@gmail.com>
2020-03-08 20:41:24 +0100

    Commit for toolbar (unfinished)

--- app/src/main/java/com/example/wikitea/HomeActivity.java
+++ app/src/main/java/com/example/wikitea/HomeActivity.java
@@ -1,10 +1,14 @@
 package com.example.wikitea;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toolbar;
 
 //Class for tea's categories
 public class HomeActivity extends AppCompatActivity implements TeaListView.TeaListener {
@@ -15,10 +19,18 @@ public class HomeActivity extends AppCompatActivity implements TeaListView.TeaLi
     String[] teadescription = {"It's green", "It's black", "It's white", "It's not a color", "I don't now what is this"};
     Integer[] imgid={R.drawable.green, R.drawable.black, R.drawable.white, R.drawable.olong, R.drawable.puer};
 
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setActionBar(toolbar);
+
+
+
 
         list = (ListView) findViewById(R.id.listview);
         TeaListView tealistview=new TeaListView(this, teaname, teadescription,imgid);
@@ -26,9 +38,27 @@ public class HomeActivity extends AppCompatActivity implements TeaListView.TeaLi
 
     }
 
+
     @Override
-    public void teaClick(int position) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Intent intent = new Intent(this, settings.class);
+                startActivity(intent);
 
 
+            case R.id.action_search:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
+
 }
